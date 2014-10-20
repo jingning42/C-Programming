b2e8fdc274f812d55104d4e7d94d2f98df54bfec
jenny42 <changchongning@gmail.com>
2014-10-20 21:08:43 +0800

    update

--- 141015/ex5.c
+++ 141015/ex5.c
@@ -5,15 +5,15 @@ main()
   float a,b,c,d,e;
   printf("Input a,b,c,d,e,\n\
 We will output the value of (a*b)/(c+d/e),\n\
-so make sure e is not 0,\n\
+so make sure the demoninator is not 0,\n\
 Please input a,b,c,d,e in order:");
   scanf("%f%f%f%f%f",&a,&b,&c,&d,&e);
   // test printf("a=%f b=%f c=%f d=%f e=%f\n",a,b,c,d,e);
-  if (e != 0)
+  if (e != 0 && c+d/e != 0 )
     {
       double y = (a*b)/(c+d/e);
       printf("y=%f\n",y);
     }
   else
-    printf("Input Error!Please make sure e is not 0\n");
+    printf("Input Error!Please make sure the demoninator is not 0\n");
 }
