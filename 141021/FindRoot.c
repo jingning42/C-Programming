02e698b15261a7a0cf8465801bb54f40b017f9bd
jenny42 <changchongning@gmail.com>
2014-10-23 21:50:43 +0800

    update

--- 141021/FindRoot.c
+++ 141021/FindRoot.c
@@ -1,33 +1,28 @@
-#include<stdio.h>
-#include<math.h>
-/* input a,b,c and find the real root of ax^2+bx+c = 0 */
+#include <stdio.h>
+#include <math.h>
+/*calcular complex root of ax^2+bx+c=0*/
 int main()
 {
-  float a,b,c;
-  printf("Please input a,b,c: ");
-  scanf("%f%f%f",&a,&b,&c);
-  double delta = b*b-4*a*c;
-  if (a==0)
-    {
-      double x;
-      if (b==0)
-        printf(c==0?"All real number is root!\n":"No real root exist!\n");
-      else
-        {
-          x = -(double)(c/b);
-          printf("x=%lf\n",x);
-        }
+  double a,b,c;
+  double x1,x2,delta,t1,t2;
+  printf("Find complex root of ax^2+bx+c=0.\n"
+         "Please input a,b,c: ");
+  scanf("%lf%lf%lf",&a,&b,&c);
+  if(a==0 && b==0)
+    printf(c==0?"All x is root!\n":"No root exist!\n");
+  else if(a==0 && b!=0)
+    printf("x=%lf\n",(double)(-c/b));
+  else{
+    delta = b*b-4*a*c;
+    t1 = -b/(2*a);
+    if (delta >= 0){
+      t2 = sqrt(delta)/(2*a);
+      printf("x1=%lf,x2=%lf\n",t1+t2,t1-t2);
     }
-  else
-    {
-      if (delta >= 0)
-        {
-          double x1 = (-b+sqrt(delta))/(2*a);
-          double x2 = (-b-sqrt(delta))/(2*a);
-          printf("x1=%lf,x2=%lf\n",x1,x2);
-        }
-      else
-        printf("No real root exist!\n");
+    else{
+      t2 = sqrt(-delta)/(2*a);
+      printf("x1=%lf+%lfi,x2=%lf%+lfi\n",t1,t2,t1,-t2);
     }
+  }
   return 0;
 }
