9101e09e41bb8b2d3fe4f11c2c50435813f910b9
Jenny <changchongning@gmail.com>
2014-11-12 10:05:26 +0800

    用3种函数输入并比较两个字符串大小

---
+++ 141112/strcompare#.c
@@ -0,0 +1,77 @@
+/* in this code we write 4 function to input a character array .
+   we can see their difference usages! */
+
+#include <stdio.h>
+
+char strcompare(char a[],char b[])
+{
+  int i;
+  for (i=0; a[i]!='\0' || b[i]!='\0'; i++)
+    {
+      if (a[i]>b[i])
+        return '>';
+      if (a[i]<b[i])
+        return '<';
+    }  
+  return '=';
+}
+
+/* scanf() */
+/* you can only input a string before space*/
+int inputarray0(char a[])
+{
+  printf("Please input a string end with 'enter': ");
+  scanf("%s",a);  
+  return 0;
+}
+
+/* getchar() */
+/* getchar() end with '\n' */
+int inputarray1(char a[])
+{
+  int i=0;
+  printf("Please input a string end with 'enter': ");
+  while ((a[i]=getchar()) != '\n')
+      i++;
+  a[i] = 0;
+  //for (i=0; a[i]!=0; i++)
+  //  printf("a[%d]=%c\n",i,a[i]);
+  printf("%s\n",a);  
+  return 0;
+}
+
+/* getchar() end with 'EOF' */
+int inputarray2(char a[])
+{
+  int i=0;
+  printf("Please input a string end with 'enter' and 'ctrl'+'D': ");
+  while ((a[i]=getchar()) != EOF)
+    {
+      printf("a[%d]=%c\n",i,a[i]);
+      i++;
+    }
+  a[i] = 0;
+  printf("%s\n",a);
+  return 0;
+}
+
+/* gets() and puts() */
+/* gets() will cause a lot of warning!*/
+int inputarray3(char a[])
+{
+  printf("Please input a string end with 'enter': ");
+  gets(a);
+  puts(a);
+  return 0;
+}
+
+
+int main()
+{
+  char a[1000]={0};
+  char b[1000]={0};
+  inputarray3(a);
+  inputarray3(b);
+  printf("%s %c %s\n", a, strcompare(a,b), b);
+  return 0;
+}
