2cbd567189a07301e4d7da6fdb240c9f4375bcd0
Jenny <changchongning@gmail.com>
2014-11-26 19:34:17 +0800

    改了下第二个人的数据，无法正确运行

--- 141126/row_pointer.c
+++ 141126/row_pointer.c
@@ -1,27 +1,27 @@
 #include<stdio.h>
 
-int *seek(int grade[]) {
+int *seek(int (*grade)[3], int j) {
     int i;
     for (i=0; i<3; i++) {
-        if (grade[i] < 60)
-            return grade;
+        if (grade[j][i] < 60)
+            return grade[j];
     }
     return NULL;
 }
 
 int main() {
     int i;
-    int grade[3][3]={{55,65,75},{65,75,85},{75,80,90}};
+    int grade[3][3]={{65,65,75},{55,75,85},{75,80,90}};
     int *fail;
     for (i=0; i<3; i++) {
-        fail = seek(grade[i]);
+      fail = seek(grade,i);
         if (fail != NULL) {
             printf("Grades of a failed student:\n");
             for (i=0; i<3; i++)
                 printf("%d ", fail[i]);
             printf("\n");
-            return 0;
         }
+        return 0;
     }
 
     printf("No failed student!\n");
